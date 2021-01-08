e0b6b30a0d2b2ee95d44a826edf689aad40c0879
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-01-08 11:30:28 +0000

    cleaner code by removing penup/pendown

--- main.py
+++ main.py
@@ -9,15 +9,12 @@ screen = t.Screen()
 def draw_line():
     for _ in range(15):
         tim.dot(30, random.randint(10, 245), random.randint(10, 245), random.randint(10, 245))
-        tim.penup()
         tim.forward(50)
-        tim.pendown()
 
 
 def draw_10_lines():
     for _ in range(15):
         draw_line()
-        tim.penup()
         tim.backward(750)
         tim.left(90)
         tim.forward(50)
@@ -31,7 +28,6 @@ def init():
     tim.right(90)
     tim.forward(350)
     tim.left(90)
-    tim.pendown()
 
 
 if __name__ == '__main__':
